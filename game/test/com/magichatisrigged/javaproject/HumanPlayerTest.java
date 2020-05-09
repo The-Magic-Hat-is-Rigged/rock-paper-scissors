15fd24365ef0344f16bda451312c00f38e6f2f2b
RJ <52873153+tupleHunden@users.noreply.github.com>
2020-05-08 21:05:08 -0400

    Added in a test file for HumanPlayer which will need some work later.

---
+++ game/test/com/magichatisrigged/javaproject/HumanPlayerTest.java
@@ -0,0 +1,31 @@
+package com.magichatisrigged.javaproject;
+
+import junit.framework.TestCase;
+
+public class HumanPlayerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() {
+    }
+
+    // This should work... but gets hung up on user entry and does not allow any input.
+    // This *does* work if you create a new humanplayer in client code and call the method... just not in JUnit.
+    public void testEnterName() {
+        HumanPlayer test = new HumanPlayer();
+        test.enterName();
+
+        assertEquals("RJ", getName());
+    }
+
+    // This should work... but gets hung up on user entry and does not allow any input.
+    // This *does* work if you create a new humanplayer in client code and call the method... just not in JUnit.
+    public void testValidMoveSelection() {
+        HumanPlayer test = new HumanPlayer();
+        test.selectMove();
+
+        assertEquals(MoveSelection.ROCK, test.getPlayerMove());
+    }
+}
