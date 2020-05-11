868022d650ae1355b9a077141b750ddd7866ba30
BruceBAWest <brucebawest@gmail.com>
2020-05-11 08:17:49 -0700

    no real update, just adding junit to test

--- game/test/com/magichatisrigged/javaproject/HumanPlayerTest.java
+++ game/test/com/magichatisrigged/javaproject/HumanPlayerTest.java
@@ -1,6 +1,7 @@
 package com.magichatisrigged.javaproject;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class HumanPlayerTest extends TestCase {
 
@@ -13,6 +14,7 @@ public class HumanPlayerTest extends TestCase {
 
     // This should work... but gets hung up on user entry and does not allow any input.
     // This *does* work if you create a new humanplayer in client code and call the method... just not in JUnit.
+    @Test
     public void testEnterName() {
         HumanPlayer test = new HumanPlayer();
         test.enterName();
@@ -22,6 +24,7 @@ public class HumanPlayerTest extends TestCase {
 
     // This should work... but gets hung up on user entry and does not allow any input.
     // This *does* work if you create a new humanplayer in client code and call the method... just not in JUnit.
+    @Test
     public void testValidMoveSelection() {
         HumanPlayer test = new HumanPlayer();
         test.selectMove();
