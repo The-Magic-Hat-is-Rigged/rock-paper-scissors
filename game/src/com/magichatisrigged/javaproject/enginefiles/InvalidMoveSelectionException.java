682ab96cc56e1294d2b981ee115cc0247ab07533
BruceBAWest <brucebawest@gmail.com>
2020-05-12 14:19:05 -0700

    add description comment at top of InvalidMoveSelectionException class

--- game/src/com/magichatisrigged/javaproject/enginefiles/InvalidMoveSelectionException.java
+++ game/src/com/magichatisrigged/javaproject/enginefiles/InvalidMoveSelectionException.java
@@ -1,5 +1,13 @@
 package com.magichatisrigged.javaproject.enginefiles;
 
+/**
+ * Custom exception InvalidMoveSelectionException class extends Exception.
+ * This exception validates user input for the com.magichatisrigged.javaproject.playerfiles.HumanPlayer class's
+ * selectMove method.
+ * This exception, combined with the while loop, try catch and switch statement in that method,
+ * ensures users input only strings that – after being parse to all capital letters – are equal to one
+ * of the allowed MoveSelection enum items
+ */
 public class InvalidMoveSelectionException extends Exception {
 
     //ctor
