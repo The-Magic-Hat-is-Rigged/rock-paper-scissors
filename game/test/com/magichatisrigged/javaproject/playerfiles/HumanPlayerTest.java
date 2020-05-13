ce073e1ea0dd65e442d327af946e0ed6978155ca
RJ <52873153+tupleHunden@users.noreply.github.com>
2020-05-13 12:20:39 -0400

    Added and updated unit testing for HumanPlayer.

---
+++ game/test/com/magichatisrigged/javaproject/playerfiles/HumanPlayerTest.java
@@ -0,0 +1,89 @@
+package com.magichatisrigged.javaproject.playerfiles;
+
+import com.magichatisrigged.javaproject.enginefiles.MoveSelection;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class HumanPlayerTest {
+
+    @Before
+    public void testSetUp()  {
+        HumanPlayer player = new HumanPlayer();
+    }
+
+    @Test
+    public void testValidEnterName() {
+        HumanPlayer player = new HumanPlayer();
+        player.setName("RJ");
+        assertEquals("RJ", player.getName());
+    }
+
+    @Test
+    public void testNotValidEnterName() {
+        HumanPlayer player = new HumanPlayer();
+        player.setName("1251234");
+        assertNotEquals("RJ", player.getName());
+    }
+
+    @Test
+    public void testValidMoveRock() {
+        HumanPlayer player = new HumanPlayer();
+        player.setPlayerMove("ROCK");
+        assertEquals(MoveSelection.ROCK, player.getPlayerMove());
+    }
+
+    @Test
+    public void testValidMoveScissors() {
+        HumanPlayer player = new HumanPlayer();
+        player.setPlayerMove("SCISSORS");
+        assertEquals(MoveSelection.SCISSORS, player.getPlayerMove());
+    }
+
+
+    @Test
+    public void testValidMovePaper() {
+        HumanPlayer player = new HumanPlayer();
+        player.setPlayerMove("PAPER");
+        assertEquals(MoveSelection.PAPER, player.getPlayerMove());
+    }
+
+    @Test
+    public void testValidMove_MoveSelectionCaseTest() {
+        HumanPlayer player = new HumanPlayer();
+        player.setPlayerMove("rOcK");
+        assertEquals(MoveSelection.ROCK, player.getPlayerMove());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNotValidMove() {
+        HumanPlayer player = new HumanPlayer();
+        player.setPlayerMove("not_rock");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNotValidMoveSpellingError() {
+        HumanPlayer player = new HumanPlayer();
+        player.setPlayerMove("rockk");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNotValidMove_IntegerInput() {
+        HumanPlayer player = new HumanPlayer();
+        player.setPlayerMove("1");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNotValidMove_DoubleInput() {
+        HumanPlayer player = new HumanPlayer();
+        player.setPlayerMove("1.001");
+    }
+
+    @After
+    public void testTearDown() {
+        HumanPlayer player = new HumanPlayer();
+    }
+}
