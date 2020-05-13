346ce4d34e49291a7988f603ea4f592b82637263
BruceBAWest <brucebawest@gmail.com>
2020-05-13 14:30:29 -0700

    create goodby message to display when exiting game

---
+++ game/src/com/magichatisrigged/javaproject/coregamefiles/GoodbyeMessage.java
@@ -0,0 +1,14 @@
+package com.magichatisrigged.javaproject.coregamefiles;
+
+public class GoodbyeMessage {
+
+    public void goodbye() {
+        System.out.println("  ________                  .______.                 \n" +
+                " /  _____/  ____   ____   __| _/\\_ |__ ___.__. ____  \n" +
+                "/   \\  ___ /  _ \\ /  _ \\ / __ |  | __ <   |  |/ __ \\ \n" +
+                "\\    \\_\\  (  <_> |  <_> ) /_/ |  | \\_\\ \\___  \\  ___/ \n" +
+                " \\______  /\\____/ \\____/\\____ |  |___  / ____|\\___  >\n" +
+                "        \\/                   \\/      \\/\\/         \\/ ");
+        System.exit(0);
+    }
+}
