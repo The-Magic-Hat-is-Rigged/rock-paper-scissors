3068655e6e77680494717f8edee5c047d646ee34
RJ <52873153+tupleHunden@users.noreply.github.com>
2020-05-13 13:02:45 -0400

    Updated HumanPlayer with advanced game options

--- game/src/com/magichatisrigged/javaproject/playerfiles/HumanPlayer.java
+++ game/src/com/magichatisrigged/javaproject/playerfiles/HumanPlayer.java
@@ -33,7 +33,6 @@ public class HumanPlayer extends Player {
         // Call printValidMoves
         System.out.println("Choose Rock, Paper, or Scissors");
 
-
         // Creates boolean validator for following while loop. this is to validate user input to only enter valid enums
         boolean isValid = false;
 
@@ -65,7 +64,57 @@ public class HumanPlayer extends Player {
                 System.out.println(e.getMessage());
             }
         }
+    }
 
+    @Override
+    public void selectMoveAdvancedGame() {
+        // This will prompt the user to select Rock, Paper, or Scissors as their move choice.
+        Scanner userMove = new Scanner(System.in);
+
+        // Call printValidMoves
+        System.out.println("Choose Rock, Paper, Scissors, Lizard, or Spock");
+
+        // Creates boolean validator for following while loop. this is to validate user input to only enter valid enums
+        boolean isValid = false;
+
+        //iterate through the user's moveSelection input
+        //if it's rock, paper or scissors, run it and exit loop, if not, ask for a proper move again
+        while (!(isValid)) {
+            try {
+                // This will parse the user input and verify it was a valid selection.
+                switch (userMove.nextLine().toUpperCase()) {
+                    case "ROCK":
+                        this.playerMove = MoveSelection.ROCK;
+                        System.out.println(getName() + " picked " + getPlayerMove());
+                        isValid = true;
+                        break;
+                    case "PAPER":
+                        this.playerMove = MoveSelection.PAPER;
+                        System.out.println(getName() + " picked " + getPlayerMove());
+                        isValid = true;
+                        break;
+                    case "SCISSORS":
+                        this.playerMove = MoveSelection.SCISSORS;
+                        System.out.println(getName() + " picked " + getPlayerMove());
+                        isValid = true;
+                        break;
+                    case "LIZARD":
+                        this.playerMove = MoveSelection.LIZARD;
+                        System.out.println(getName() + " picked " + getPlayerMove());
+                        isValid = true;
+                        break;
+                    case "SPOCK":
+                        this.playerMove = MoveSelection.SPOCK;
+                        System.out.println(getName() + " picked " + getPlayerMove());
+                        isValid = true;
+                        break;
+                    default:
+                        throw new InvalidMoveSelectionException();
+                }
+            } catch(InvalidMoveSelectionException e){
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public MoveSelection getPlayerMove () {
