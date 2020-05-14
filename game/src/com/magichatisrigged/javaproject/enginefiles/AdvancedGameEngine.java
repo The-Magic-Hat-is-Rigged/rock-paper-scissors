9055e9c1b626201e7b837100a40e18377974b279
RJ <52873153+tupleHunden@users.noreply.github.com>
2020-05-14 09:28:27 -0400

    Added comments and refactored.

--- game/src/com/magichatisrigged/javaproject/enginefiles/AdvancedGameEngine.java
+++ game/src/com/magichatisrigged/javaproject/enginefiles/AdvancedGameEngine.java
@@ -19,28 +19,11 @@ import java.util.Scanner;
 public class AdvancedGameEngine extends GameEngine {
     public static final int MIN_GAME_ROUNDS = 1;
     public static final int MAX_GAME_ROUNDS = 50;
+
     private int numberOfGameRounds;
     private int gameRoundCounter = 1;
 
-    /*
-     * Constructor methods for GameEngine
-     */
-    //no-op ctor
-    public AdvancedGameEngine() {
-        //no-op ctor
-    }
-
-    //one arg ctor for num of game rounds to play
-    public AdvancedGameEngine(int numberOfGameRounds) {
-        this.numberOfGameRounds = numberOfGameRounds;
-    }
-
-    //two arg ctor for test purposes, tests numOfGames and gameRoundCounter
-    public AdvancedGameEngine(int numberOfGameRounds, int gameRoundCounter) {
-        this.numberOfGameRounds = numberOfGameRounds;
-        this.gameRoundCounter = gameRoundCounter;
-    }
-
+    // This is the main logic method for the game and will run the advanced version of Rock-Paper-Scissors.
     @Override
     public void playGame() {
         HumanPlayer humanPlayer = new HumanPlayer();
@@ -51,62 +34,47 @@ public class AdvancedGameEngine extends GameEngine {
         enterNumberOfGameRounds();
         enterNamePrompt(humanPlayer, computerPlayer);
 
-        // The number of lives for both the human and the computer will be based on the user provided input above.
         int humanWinCounter = 0;
         int computerWinCounter = 0;
 
-        // This will loop "n" times based on the number of games the player selected at the start.
         for (int i = 0; i < getNumberOfGameRounds(); i++) {
-
-            // This will display which game round is currently in play.
             System.out.println("\n----- Round Number: " + getGameRoundCounter() + " -----");
 
-            // These methods will prompt the user and computer to make their moves.
             humanPlayer.selectMoveAdvancedGame();
             computerPlayer.selectMoveAdvancedGame();
 
-            // This if block will execute if the human player loses the round and take one away from the humans life pool.
             if (humanPlayer.getPlayerMove().losesTo(computerPlayer.getComputerMove())) {
                 System.out.println(computerPlayer.getName() + " wins round " + getGameRoundCounter() + "!");
                 computerWinCounter++;
                 scoreBoardDisplay(humanPlayer, computerPlayer, humanWinCounter, computerWinCounter);
             }
-
-            // This if block will execute if the computer player loses the round and take one away from the computer life pool.
             else if (computerPlayer.getComputerMove().losesTo(humanPlayer.getPlayerMove())) {
                 System.out.println(humanPlayer.getName() + " wins round " + getGameRoundCounter() + "!");
                 humanWinCounter++;
                 scoreBoardDisplay(humanPlayer, computerPlayer, humanWinCounter, computerWinCounter);
             }
-
-            // The only other option besides win or lose would be tie, in which case no lives will be taken from either the human or computer.
             else {
                 tieMatchTextDisplay();
                 numberOfGameRounds++;
             }
-
-            // This will increase the game round counter by one.
             gameRoundCounter++;
         }
 
-        // This if else statement will display the final winner of the game based on who has won more rounds.
+        // This will display word art for the player depending on if they won or lost the game.
         if (humanWinCounter > computerWinCounter) {
             humanWinTextDisplay(humanPlayer);
         }
-
         else {
             humanLoseTextDisplay(computerPlayer);
         }
-        //selection prompt to play game again, or return to menu, defined below
         playAgainPrompt();
     }
 
-    /*
-     * playAgainPrompt gives you choide of replaying game, or returning to menu
-     */
+    // This will prompt the user asking if they would like to play again, or exit to the game menu.
     private void playAgainPrompt() {
         System.out.println("Would you like to replay the game?\n" +
                            "Type Yes to replay, or type Exit to return to menu.");
+
         Scanner gameSelection = new Scanner(System.in);
         boolean isValid = false;
         while (!(isValid)) {
@@ -129,23 +97,16 @@ public class AdvancedGameEngine extends GameEngine {
                 System.out.println(e.getMessage());
             }
         }
-    }
+   }
 
-    /*
-     * private method for setting the number of games to be played
-     * This integer can be between 1 and 50 game rounds (although, total play count can exceed this in the event of ties).
-     * set between MIN_GAME_ROUNDS && MAX_GAME_ROUNDS
-     * this method is called by the playGame method, set to private because only the play came method can see it.
-     */
-    private void enterNumberOfGameRounds() {
-        // This will ask the user how many rounds per game they would like to play.
+   // This will prompt the user asking how many game rounds they would like to play for the current game.
+   private void enterNumberOfGameRounds() {
         System.out.println("How many rounds would you like to play this game? \n");
-        // This will ask how many games the user would like to play.
+
         Scanner userInput = new Scanner(System.in);
-        //create reusable string to return when user inputs invalid number of rounds
         String printValidNumRange = "Please enter a number between: " + MIN_GAME_ROUNDS + " and " + MAX_GAME_ROUNDS + ".";
         boolean isValid = false;
-        //DONE: explore do/while, if/else to set playCount, so instead of breaking game, player is prompted to select a proper playcount
+
         while (!(isValid)) {
             System.out.println(printValidNumRange);
             try {
@@ -155,13 +116,13 @@ public class AdvancedGameEngine extends GameEngine {
                     numberOfGameRounds = userInputConvertedFromString;
                     System.out.println("You will play " + getNumberOfGameRounds() + " rounds this game.");
                 }
-            } catch (NumberFormatException e) {
+            }
+            catch (NumberFormatException e) {
                 System.out.println("Invalid number. " + printValidNumRange);
             }
         }
     }
 
-    // Public getter for the number of games.
     public int getNumberOfGameRounds() {
         return numberOfGameRounds;
     }
